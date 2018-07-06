19839963e0712f2fbfa6d23f22fefda08b4af326
Jeff Schwab <jeff@unbuggy.com>
2018-07-06 18:24:52 -0400

    Solve Problem 1: Multiples of 3 and 5

---
+++ README.md
@@ -0,0 +1,6 @@
+# rust-euler
+
+This repository contains [Rust][] solutions to some of the [Project Euler][] problems.
+
+[Rust]: https://www.rust-lang.org/en-US/
+[Project Euler]: https://projecteuler.net/
---
+++ src/problem1.rs
@@ -0,0 +1,17 @@
+//! Multiples of 3 and 5
+
+/// Returns the sum of all multiples of factor in the range 1..end.
+fn sum_multiples(factor: i32, end: i32) -> i32 {
+    let count = (end - 1) / factor;
+    let multiplicand = count * (count + 1) / 2; // sum of 1..end
+    multiplicand * factor
+}
+
+/// Returns the sum of all multiples of 3 or 5 in the range 1..1000.
+///
+///     assert_eq!(rust_euler::problem1::solution(), 233168);
+///
+pub fn solution() -> i32 {
+    let f = |n| sum_multiples(n, 1000);
+    f(3) + f(5) - f(15)
+}
