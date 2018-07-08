f17c927ad76db9d69952e69a5e37b85c89c146e9
Jeff Schwab <jeff@unbuggy.com>
2018-07-08 08:05:06 -0400

    Enable trivial benchmarks
        rust-euler $ rustup run nightly cargo bench
           Compiling rust-euler v0.1.0 (file:///Users/jeff/git/rust-euler)
            Finished release [optimized] target(s) in 1.20s
             Running target/release/deps/rust_euler-6a0b4265fcd4c044
    
        running 15 tests
        test bit_vec::tests::test_new ... ignored
        test bit_vec::tests::test_pushn ... ignored
        test bit_vec::tests::test_with_all ... ignored
        test problem1::tests::test_solution ... ignored
        test problem2::tests::test_solution ... ignored
        test problem3::tests::test_solution ... ignored
        test problem4::tests::test_solution ... ignored
        test problem4::tests::test_to_digits_reverse ... ignored
        test problem5::tests::test_solution ... ignored
        test problem6::tests::test_solution ... ignored
        test problem7::tests::test_solution ... ignored
        test tests::bench_prime_generate10    ... bench:         703 ns/iter (+/- 156)
        test tests::bench_prime_generate100   ... bench:      51,456 ns/iter (+/- 11,401)
        test tests::bench_prime_generate1000  ... bench:   4,664,466 ns/iter (+/- 856,015)
        test tests::bench_prime_generate10000 ... bench: 449,674,096 ns/iter (+/- 15,188,035)
    
        test result: ok. 0 passed; 0 failed; 11 ignored; 4 measured; 0 filtered out

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+#![feature(test)]
+
+extern crate test;
+
 pub mod problem1;
 pub mod problem2;
 pub mod problem3;
@@ -8,3 +12,30 @@ pub mod problem7;
 
 pub mod bit_vec;
 pub mod prime;
+
+#[cfg(test)]
+mod tests {
+    use test::Bencher;
+
+    use prime;
+
+    #[bench]
+    fn bench_prime_generate10(b: &mut Bencher) {
+        b.iter(|| { prime::generate(10); });
+    }
+
+    #[bench]
+    fn bench_prime_generate100(b: &mut Bencher) {
+        b.iter(|| { prime::generate(100); });
+    }
+
+    #[bench]
+    fn bench_prime_generate1000(b: &mut Bencher) {
+        b.iter(|| { prime::generate(1000); });
+    }
+
+    #[bench]
+    fn bench_prime_generate10000(b: &mut Bencher) {
+        b.iter(|| { prime::generate(10000); });
+    }
+}
