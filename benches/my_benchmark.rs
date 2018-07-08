f963517f85f1849cd5fe1366de1536e9ec422ca8
Jeff Schwab <jeff@unbuggy.com>
2018-07-08 09:06:58 -0400

    Benchmark prime::generate
        rust-euler $ cargo bench
           Compiling rust-euler v0.1.0 (file:///Users/jeff/git/rust-euler)
            Finished release [optimized] target(s) in 1.23s
             Running target/release/deps/rust_euler-741b0ef74e54eed6
    
        running 11 tests
        test bit_vec::tests::test_new ... ignored
        test bit_vec::tests::test_pushn ... ignored
        test bit_vec::tests::test_with_all ... ignored
        test problem1::tests::test_solution ... ignored
        test problem2::tests::test_solution ... ignored
        test problem3::tests::test_solution ... ignored
        test problem4::tests::test_solution ... ignored
        test problem4::tests::test_to_digits_reverse ... ignored
        test problem5::tests::test_solution ... ignored
        test problem6::tests::test_solution ... ignored
        test problem7::tests::test_solution ... ignored
    
        test result: ok. 0 passed; 0 failed; 11 ignored; 0 measured; 0 filtered out
    
             Running target/release/deps/my_benchmark-e2398e7ff2bbfcde
        Gnuplot not found, disabling plotting
        fib 20                  time:   [25.765 us 26.054 us 26.332 us]
                                change: [-0.0431% +2.2456% +4.7511%] (p = 0.08 > 0.05)
                                No change in performance detected.
        Found 7 outliers among 100 measurements (7.00%)
          5 (5.00%) high mild
          2 (2.00%) high severe
    
        prime generate 10       time:   [695.73 ns 703.20 ns 710.58 ns]
                                change: [-3.7701% -1.8142% +0.1509%] (p = 0.08 > 0.05)
                                No change in performance detected.
        Found 3 outliers among 100 measurements (3.00%)
          3 (3.00%) high mild
    
        prime generate 100      time:   [49.386 us 49.980 us 50.596 us]
        Found 5 outliers among 100 measurements (5.00%)
          2 (2.00%) high mild
          3 (3.00%) high severe
    
        prime generate 1000     time:   [4.5615 ms 4.5835 ms 4.6060 ms]
        Found 3 outliers among 100 measurements (3.00%)
          2 (2.00%) high mild
          1 (1.00%) high severe
    
        prime generate 10000    time:   [447.13 ms 448.96 ms 450.62 ms]
    
        Gnuplot not found, disabling plotting
    
    rust-euler $

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -1,13 +1,17 @@
 #[macro_use]
 extern crate criterion;
 
+extern crate rust_euler;
+
+use rust_euler::prime;
+
 use criterion::Criterion;
 
 fn fibonacci(n: u64) -> u64 {
     match n {
         0 => 1,
         1 => 1,
-        n => fibonacci(n-1) + fibonacci(n-2),
+        n => fibonacci(n - 1) + fibonacci(n - 2),
     }
 }
 
@@ -15,5 +19,31 @@ fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("fib 20", |b| b.iter(|| fibonacci(20)));
 }
 
-criterion_group!(benches, criterion_benchmark);
+fn criterion_benchmark_prime_generate10(c: &mut Criterion) {
+    c.bench_function("prime generate 10", |b| b.iter(|| prime::generate(10)));
+}
+
+fn criterion_benchmark_prime_generate100(c: &mut Criterion) {
+    c.bench_function("prime generate 100", |b| b.iter(|| prime::generate(100)));
+}
+
+fn criterion_benchmark_prime_generate1000(c: &mut Criterion) {
+    c.bench_function("prime generate 1000", |b| b.iter(|| prime::generate(1000)));
+}
+
+fn criterion_benchmark_prime_generate10000(c: &mut Criterion) {
+    c.bench_function("prime generate 10000", |b| {
+        b.iter(|| prime::generate(10000))
+    });
+}
+
+criterion_group!(
+    benches,
+    criterion_benchmark,
+    criterion_benchmark_prime_generate10,
+    criterion_benchmark_prime_generate100,
+    criterion_benchmark_prime_generate1000,
+    criterion_benchmark_prime_generate10000,
+);
+
 criterion_main!(benches);
